ca3f1cca9c6e8ee1814bb591083ea310308eaafb
Volodymyr the destroyer of dwarf <152634784+VladtheGeneral@users.noreply.github.com>
2023-12-16 11:14:27 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    Vlad_legend += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Vlad_legend)
+})
+input.onButtonPressed(Button.B, function () {
+    Vlad_legend += -1
+})
+let Vlad_legend = 0
+Vlad_legend = 0
+basic.forever(function () {
+	
+})
